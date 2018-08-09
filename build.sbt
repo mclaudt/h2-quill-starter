6a5b4da2bfe654c93520449d1cfa8c59de53f2da
mclaudt <mclaudt@gmail.com>
2018-08-09 23:54:10 +0300

    added quill simple operations

--- build.sbt
+++ build.sbt
@@ -5,7 +5,9 @@ version := "1.0-SNAPSHOT"
 scalaVersion := "2.12.2"
 
 libraryDependencies ++= List(
-  "com.h2database" % "h2" % "1.4.192"
+  "com.h2database" % "h2" % "1.4.192",
+  "io.getquill" %% "quill-jdbc" % "2.5.4"
+
 )
 
 scalacOptions ++= Seq("-Xfatal-warnings", "-deprecation", "-print", "-unchecked", "-feature", "-Xlint", "-Ywarn-inaccessible", "-Ywarn-nullary-override", "-Ywarn-nullary-unit")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
-
-
+import com.typesafe.config.ConfigFactory
+import io.getquill.H2JdbcContext
+import io.getquill._
 object Main extends App {
 
 
@@ -7,9 +8,42 @@ object Main extends App {
 
   import org.h2.tools.Server
 
+
   Server.createTcpServer("-tcpAllowOthers").start
   Class.forName("org.h2.Driver")
+      //Это подключение - особое - оно поднимает саму базу. Потом будет подключение сюда же, но уже как обычный пользователь.
   DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test;MODE=PostgreSQL;AUTO_SERVER=TRUE", "sa", "")
 
   Server.createWebServer().start()
+//  Поднимется веб интерфейс по адресу:
+//  http://localhost:8082
+
+  //Подключаемся уже как бы извне и создаем таблицу
+  implicit lazy val ctx: H2JdbcContext[SnakeCase] = new H2JdbcContext(
+    SnakeCase,
+    ConfigFactory.parseString(
+      s"""{"driverClassName":"org.h2.Driver","jdbcUrl":"jdbc:h2:file:~/test;USER=sa;PASSWORD="}"""
+    )
+  )
+
+
+
+  ctx.executeAction(
+    """CREATE TABLE todo (
+      |  checked BOOLEAN
+      |);
+      |""".stripMargin
+  )
+
+  case class Todo(checked: Boolean)
+
+  import ctx._
+  run(query[Todo].insert(_.checked -> false))
+
+
+    //Два вопроса - mem невозмиожен в данном виде?
+
+  //Можно ли подрубиться через постгресовский драйвер - по идее да
+
+
 }
