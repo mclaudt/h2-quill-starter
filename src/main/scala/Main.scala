d3c83c84863779f95abff8e23a3c804f849b19fe
mclaudt <mclaudt@gmail.com>
2018-11-16 08:18:15 +0300

    refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,49 +1,52 @@
+import java.util.Calendar
 import com.typesafe.config.ConfigFactory
 import io.getquill.H2JdbcContext
 import io.getquill._
+import java.sql.DriverManager
+import org.h2.tools.Server
+
+
 object Main extends App {
 
-
-  import java.sql.DriverManager
-
-  import org.h2.tools.Server
-
+  // ---------------------- Server code --------------------------
 
   Server.createTcpServer("-tcpAllowOthers").start
+
   Class.forName("org.h2.Driver")
-      //Это подключение - особое - оно поднимает саму базу. Потом будет подключение сюда же, но уже как обычный пользователь.
-  DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test;MODE=PostgreSQL;AUTO_SERVER=TRUE", "sa", "")
+
+  // Connection to create the whole database
+  DriverManager.getConnection("jdbc:h2:tcp://localhost/~/h2test;MODE=PostgreSQL;AUTO_SERVER=TRUE", "sa", "")
 
   Server.createWebServer().start()
-//  Поднимется веб интерфейс по адресу:
-//  http://localhost:8082
+  //  WEB interface is up on address:
+  //  http://localhost:8082
 
-  //Подключаемся уже как бы извне и создаем таблицу
+
+
+  // ---------------------- Client code --------------------------
+
   implicit lazy val ctx: H2JdbcContext[SnakeCase] = new H2JdbcContext(
     SnakeCase,
     ConfigFactory.parseString(
-      s"""{"driverClassName":"org.h2.Driver","jdbcUrl":"jdbc:h2:file:~/test;USER=sa;PASSWORD="}"""
+      s"""{"driverClassName":"org.h2.Driver","jdbcUrl":"jdbc:h2:file:~/h2test;USER=sa;PASSWORD="}"""
     )
   )
 
-
-
+  // file is persisted, that's why "if not exists"
   ctx.executeAction(
-    """CREATE TABLE todo (
-      |  checked BOOLEAN
+    """CREATE TABLE IF NOT EXISTS todo (
+      |  checked BOOLEAN,
+      |  date VARCHAR
       |);
       |""".stripMargin
   )
 
-  case class Todo(checked: Boolean)
+  case class Todo(checked: Boolean,date:String)
 
   import ctx._
-  run(query[Todo].insert(_.checked -> false))
 
+  run(query[Todo].insert(_.checked -> true, _.date-> lift(Calendar.getInstance().getTime.toString)))
 
-    //Два вопроса - mem невозмиожен в данном виде?
-
-  //Можно ли подрубиться через постгресовский драйвер - по идее да
-
+  println(run(query[Todo].filter(_.checked==true)))
 
 }
